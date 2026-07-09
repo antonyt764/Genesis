d9ce597db045b5e2c53f5229768e346db0631c05
tony555t <your-email@example.com>
2026-07-09 10:18:31 +0300

    modules

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='posts',
+    )
+    excerpt = models.TextField(blank=True)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog/', blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-created_at']  # newest first
+
+    def __str__(self):
+        return self.title
